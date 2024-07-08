3fc72a0991977abd667a60b0debd4564a8ab7264
Niyozbek Mirzayev <n.mirzayev@baik.uz>
2024-07-08 14:19:58 +0500

    StringExtensions added

---
+++ CoreProjectLib/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CoreProjectLib.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string GetDigits(this string input) 
+        {
+            return new string(input.Where(c => char.IsDigit(c)).ToArray());
+        }
+    }
+}
