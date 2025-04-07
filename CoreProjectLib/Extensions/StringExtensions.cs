b40166b4a93d9fc338b982cd1a708e6e8da8038b
Niyozbek Mirzayev <niyozbek.mirzayev@ssd.uz>
2025-04-07 12:21:59 +0500

    Add ToSHA256 method to StringExtensions class
    Introduce ToSHA256 extension method in StringExtensions class to compute SHA-256 hash of a string

--- CoreProjectLib/Extensions/StringExtensions.cs
+++ CoreProjectLib/Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Security.Cryptography;
+using System.Text;
+
 namespace CoreProjectLib.Extensions
 {
     public static class StringExtensions
@@ -7,5 +9,23 @@ namespace CoreProjectLib.Extensions
         {
             return new string(input.Where(c => char.IsDigit(c)).ToArray());
         }
+
+        public static string ToSHA256(this string text)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(text);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
     }
 }
